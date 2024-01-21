3e525d2c5b991d81bd85a09b54306024cf778dc8
Michael Chirlin <michael.chirlin@appiancorp.com>
2024-01-21 16:41:46 -0500

    List

---
+++ web_admin/frontend/src/App-old.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/assets/vite.svg'
+import './App.css'
+
+function App() {
+  // usestate for setting a javascript
+  // object for storing and using status
+  const [status, setstatus] = useState({
+    game_mode: ""
+  });
+
+  // Using useEffect for single rendering
+  useEffect(() => {
+  		fetch("/status").then((res) =>
+  			res.json().then((status) => {
+  				// Setting status from api
+  				setstatus({
+  					game_mode: status.game_mode,
+  				});
+  			})
+  		);
+  	}, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <header className="App-header">
+        <h1>React and flask</h1>
+        {/* Calling /status from setstatus for showing */}
+        <p>{status.game_mode}</p>
+      </header>
+    </>
+  )
+}
+
+export default App
--- web_admin/frontend/src/App.jsx
+++ web_admin/frontend/src/App.jsx
@@ -1,45 +1,92 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/assets/vite.svg'
-import './App.css'
+import * as React from 'react';
+import List from '@mui/material/List';
+import Box from '@mui/material/Box';
+import ListItem from '@mui/material/ListItem';
+import Paper from '@mui/material/Paper';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import Typography from '@mui/material/Typography';
+import {
+  Link as RouterLink,
+  LinkProps as RouterLinkProps,
+  Route,
+  Routes,
+  MemoryRouter,
+  useLocation,
+} from 'react-router-dom';
+import { StaticRouter } from 'react-router-dom/server';
 
-function App() {
-  // usestate for setting a javascript
-  // object for storing and using status
-  const [status, setstatus] = useState({
-    game_mode: ""
-  });
-
-  // Using useEffect for single rendering
-  useEffect(() => {
-  		fetch("/status").then((res) =>
-  			res.json().then((status) => {
-  				// Setting status from api
-  				setstatus({
-  					game_mode: status.game_mode,
-  				});
-  			})
-  		);
-  	}, []);
+function Router(props: { children?: React.ReactNode }) {
+  const { children } = props;
+  if (typeof window === 'undefined') {
+    return <StaticRouter location="/drafts">{children}</StaticRouter>;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <header className="App-header">
-        <h1>React and flask</h1>
-        {/* Calling /status from setstatus for showing */}
-        <p>{status.game_mode}</p>
-      </header>
-    </>
-  )
+    <MemoryRouter initialEntries={['/drafts']} initialIndex={0}>
+      {children}
+    </MemoryRouter>
+  );
 }
 
-export default App
+interface ListItemLinkProps {
+  icon?: React.ReactElement;
+  primary: string;
+  to: string;
+}
+
+const Link = React.forwardRef<HTMLAnchorElement, RouterLinkProps>(function Link(
+  itemProps,
+  ref,
+) {
+  return <RouterLink ref={ref} {...itemProps} role={undefined} />;
+});
+
+function ListItemLink(props: ListItemLinkProps) {
+  const { icon, primary, to } = props;
+
+  return (
+    <li>
+      <ListItem button component={Link} to={to}>
+        {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
+        <ListItemText primary={primary} />
+      </ListItem>
+    </li>
+  );
+}
+
+function Content() {
+  const location = useLocation();
+  return (
+    <Typography variant="body2" sx={{ pb: 2 }} color="text.secondary">
+      Current route: {location.pathname}
+    </Typography>
+  );
+}
+
+export default function ListRouter() {
+  return (
+    <Router>
+      <Box sx={{ width: 360 }}>
+        <Routes>
+          <Route path="*" element={<Content />} />
+        </Routes>
+
+        <Paper elevation={0}>
+          <List aria-label="main mailbox folders">
+            <ListItemLink to="/inbox" primary="Inbox" icon={<InboxIcon />} />
+            <ListItemLink to="/drafts" primary="Drafts" icon={<DraftsIcon />} />
+          </List>
+          <Divider />
+          <List aria-label="secondary mailbox folders">
+            <ListItemLink to="/trash" primary="Trash" />
+            <ListItemLink to="/spam" primary="Spam" />
+          </List>
+        </Paper>
+      </Box>
+    </Router>
+  );
+}
