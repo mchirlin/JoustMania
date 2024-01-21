e6049972bb53d10b13c57009275bb87f5d54d871
Michael Chirlin <michael.chirlin@appiancorp.com>
2024-01-21 16:25:00 -0500

    Update App.jsx

--- web_admin/frontend/src/App.jsx
+++ web_admin/frontend/src/App.jsx
@@ -1,35 +1,45 @@
-// Filename - App.js
-
-// Importing modules
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-	// usestate for setting a javascript
-	// object for storing and using data
-	const [status, setstatus] = useState();
+  // usestate for setting a javascript
+  // object for storing and using status
+  const [status, setstatus] = useState({
+    game_mode: ""
+  });
 
-	// Using useEffect for single rendering
-	useEffect(() => {
-		// Using fetch to fetch the api from
-		// flask server it will be redirected to proxy
-		fetch("/status").then((res) =>
-			res.json().then((status) => {
-				// Setting a data from api
-				setstatus(status);
-			})
-		);
-	}, []);
+  // Using useEffect for single rendering
+  useEffect(() => {
+  		fetch("/status").then((res) =>
+  			res.json().then((status) => {
+  				// Setting status from api
+  				setstatus({
+  					game_mode: status.game_mode,
+  				});
+  			})
+  		);
+  	}, []);
 
-	return (
-		<div className="App">
-			<header className="App-header">
-				<h1>React and flask</h1>
-				{/* Displaying data */}
-				<p>{status}</p>
-			</header>
-		</div>
-	);
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <header className="App-header">
+        <h1>React and flask</h1>
+        {/* Calling /status from setstatus for showing */}
+        <p>{status.game_mode}</p>
+      </header>
+    </>
+  )
 }
 
-export default App;
+export default App
