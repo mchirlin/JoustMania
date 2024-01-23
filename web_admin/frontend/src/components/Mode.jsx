1b7f8c17cad37d256c1d0a211f99eb0e4cad80bd
Michael Chirlin <michael.chirlin@appiancorp.com>
2024-01-22 23:17:40 -0500

    Update Mode.jsx

--- web_admin/frontend/src/components/Mode.jsx
+++ web_admin/frontend/src/components/Mode.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import IconButton from '@mui/material/IconButton';
