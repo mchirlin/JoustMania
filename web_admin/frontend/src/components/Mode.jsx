760bdb8e1377a840fff963524062be535ed1a4bb
Michael Chirlin <michael.chirlin@appiancorp.com>
2024-01-22 23:28:21 -0500

    Update Mode.jsx

--- web_admin/frontend/src/components/Mode.jsx
+++ web_admin/frontend/src/components/Mode.jsx
@@ -14,50 +14,17 @@ import {
     CardMedia,
 } from "@mui/material";
 
-const icons = [
-  { icon: <HomeIcon style={{ fontSize: 100 }}/>, label: 'Home' },
-  { icon: <SportsEsportsIcon style={{ fontSize: 100 }}/>, label: 'Mode' },
-  { icon: <Battery4BarIcon style={{ fontSize: 100 }}/>, label: 'Controllers' },
-  { icon: <SettingsIcon style={{ fontSize: 100 }}/>, label: 'Settings' },
-  { icon: <HomeIcon style={{ fontSize: 100 }}/>, label: 'Home' },
-  { icon: <SportsEsportsIcon style={{ fontSize: 100 }}/>, label: 'Mode' },
-  { icon: <Battery4BarIcon style={{ fontSize: 100 }}/>, label: 'Controllers' },
-  { icon: <SettingsIcon style={{ fontSize: 100 }}/>, label: 'Settings' },
-  { icon: <HomeIcon style={{ fontSize: 100 }}/>, label: 'Home' },
-  { icon: <SportsEsportsIcon style={{ fontSize: 100 }}/>, label: 'Mode' },
-  { icon: <Battery4BarIcon style={{ fontSize: 100 }}/>, label: 'Controllers' },
-  { icon: <SettingsIcon style={{ fontSize: 100 }}/>, label: 'Settings' },
-];
-
 export default function Home() {
-
-  const [status, setstatus] = useState({
-    game_mode: ""
-  });
 
   const [modes, setmodes] = useState({
     modes: []
   });
 
-  // Using useEffect for single rendering
-  useEffect(() => {
-  		fetch("/status").then((res) =>
-  			res.json().then((status) => {
-  				// Setting status from api
-  				setstatus({
-  					game_mode: status.game_mode,
-  				});
-  			})
-  		);
-  	}, []);
-
    useEffect(() => {
    		fetch("/game_modes").then((res) =>
    			res.json().then((modes) => {
-   				// Setting status from api
-   				setmodes({
-   					game_modes: modes,
-   				});
+   				// Getting modes from backend server
+   				setmodes(modes);
    			})
    		);
    	}, []);
@@ -70,17 +37,9 @@ export default function Home() {
             <Grid item key={index}>
              <Card sx={{ maxWidth: 345 }}>
                <CardActionArea>
-  {/*                         <CardMedia */}
-  {/*                             component="img" */}
-  {/*                             height="180" */}
-  {/*                             image= */}
-  {/*  "https://media.geeksforgeeks.org/wp-content/uploads/20220221132017/download.png" */}
-  {/*                             alt="gfg" */}
-  {/*                         /> */}
-                   <CardContent>
-                    <div>{item.name}</div>
-{/*                      {item.icon} */}
-                   </CardContent>
+                 <CardContent>
+                  <div>{item.name}</div>
+                 </CardContent>
                </CardActionArea>
               </Card>
             </Grid>
