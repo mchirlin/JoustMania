f704ab32542d4c2cbb8467a2b259cb3d6c7da9e9
Michael Chirlin <michael.chirlin@appiancorp.com>
2024-01-23 00:56:32 -0500

    Update server.py

--- server.py
+++ server.py
@@ -54,7 +54,7 @@ class Server:
 
       @self.app.route('/game_modes')
       def game_modes():
-          # print(json.dumps([{e.pretty_name} for e in common.Games]))
+          print([e.value for e in common.Games])
           return json.dumps(dict())
 
     def web_loop(self):
