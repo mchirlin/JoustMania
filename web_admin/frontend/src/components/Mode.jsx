357655f48d14b2a97df92a9c4a500efe4347e3af
Michael Chirlin <michael.chirlin@appiancorp.com>
2024-01-22 23:33:07 -0500

    asdf

--- web_admin/frontend/src/components/Mode.jsx
+++ web_admin/frontend/src/components/Mode.jsx
@@ -1,7 +1,4 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/assets/vite.svg'
-import './App.css'
 
 export default function Mode() {
   // usestate for setting a javascript
@@ -24,14 +21,6 @@ export default function Mode() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <header className="App-header">
         <h1>React and flask</h1>
